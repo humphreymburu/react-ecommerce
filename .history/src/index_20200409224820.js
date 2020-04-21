2b98e5b502326adc44809bb4a1d80404f32379ba
humphreymburu <humphrey.asante@gmail.com>
2020-04-21 19:38:05 +0300

    added styling

--- .history/src/index_20200409224820.js
+++ .history/src/index_20200409224820.js
@@ -3,13 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {createStore} from 'redux'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
-
-const store = createStore()
 
 ReactDOM.render(
   <React.StrictMode>
