430280f3b0f24576ac97b839a7a403ac964210a3
humphreymburu <humphrey.asante@gmail.com>
2020-05-11 19:44:00 +0300

    search added

---
+++ .history/src/index_20200511122610.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/css/App.css';
+import routes from './node_modules/routes'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Provider } from 'react-redux'
+import { ConnectedRouter } from 'connected-react-router'
+import configureStore, { history } from './node_modules/configureStore'
+
+const store = configureStore(/* provide initial state if any */)
+
+ReactDOM.render(
+<Provider store={store}>
+    <ConnectedRouter history={history}> { /* place ConnectedRouter under Provider */ }
+      <> 
+      { /* your usual react-router v4/v5 routing */ }
+        
+      {routes}
+      </>
+    </ConnectedRouter>
+  </Provider>,
+  document.getElementById('root')
+);
